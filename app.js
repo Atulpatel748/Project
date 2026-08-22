4b09afa90ebbe8a4e07f680b044c793c3c80bbf7
Addiction023 <atulpatel09876@gmail.com>
2026-08-22 15:35:03 +0530

    added create route

--- app.js
+++ app.js
@@ -19,14 +19,29 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am Groot");
 });
 
-app.get("/listing", async (req, res) => {
+app.get("/listings", async (req, res) => {
   const allListing = await Listing.find({});
   res.render("listings/index.ejs", { allListing });
+});
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 // app.get("/testListing", async (req, res) => {
@@ -44,5 +59,5 @@ app.get("/listing", async (req, res) => {
 // });
 
 app.listen(8080, () => {
-  console.log("Server is listening to port 3000");
+  console.log("Server is listening to port 8080");
 });
