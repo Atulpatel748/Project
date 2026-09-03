fc7dd1fe055dd9db01787cceb7d96c06d17fd0ca
Addiction023 <atulpatel09876@gmail.com>
2026-09-03 13:46:53 +0530

    few update

---
+++ models/review.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the Review schema
+const reviewSchema = new Schema({
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
