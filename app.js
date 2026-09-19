368cb917951b16c6f60ed657b51cc3dab8f580b1
Addiction023 <atulpatel09876@gmail.com>
2026-09-19 18:19:36 +0530

    Remove obsolete compatibility code and rating styles

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const path = require("path");
-const util = require("util");
-
-if (util.isArray !== Array.isArray) {
-  util.isArray = Array.isArray;
-}
-
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
